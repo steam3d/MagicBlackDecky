db2dee658dce4d39abe17b2b6322300c397e3e2f
Alexander Maslov <steam3d@gmail.com>
2024-03-27 16:33:19 +0300

    Added new button icons

--- src/ButtonIcons.tsx
+++ src/ButtonIcons.tsx
@@ -20,3 +20,17 @@ export const SELECT = ({ style }: { style?: CSSProperties | undefined }) => {
         <path d="m7.33,8.67c-4.05,0-7.33,3.28-7.33,7.33s3.28,7.33,7.33,7.33h17.33c4.05,0,7.33-3.28,7.33-7.33s-3.28-7.33-7.33-7.33H7.33Zm.27,3.06h10.27v1.71h-8.4v3.42h3.73v1.71h-5.6v-6.84Zm7.47,3.42h9.33v5.13h-9.33v-5.13Z" fill-rule="evenodd" clip-rule="evenodd" fill="currentColor" />
     </svg>
 }
+
+export const L5 = ({ style }: { style?: CSSProperties | undefined }) => {
+    return <svg style={style} width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <rect width="32" height="32" fill="none" />
+        <path d="m2,0C.9,0,0,.9,0,2v28c0,1.1.9,2,2,2h28c1.1,0,2-.9,2-2V2c0-1.1-.9-2-2-2H2Zm5.32,22h7.52v-2.11h-5.17v-9.09h-2.35v11.2Zm17.01-9.2v-2h-7.15v5.78l1.49.82c.33-.27.67-.46,1.01-.59.34-.14.71-.21,1.12-.21.54,0,.97.14,1.28.43.32.29.48.7.48,1.25,0,.62-.21,1.09-.62,1.42-.42.33-1,.5-1.76.5-1,0-1.91-.28-2.74-.83l-.83,1.79c.42.35.94.62,1.57.82.63.19,1.31.29,2.03.29.98,0,1.83-.17,2.54-.5.73-.34,1.27-.82,1.65-1.42.38-.61.58-1.32.58-2.13,0-1.09-.3-1.91-.91-2.48-.6-.58-1.4-.86-2.4-.86-.39,0-.79.05-1.18.14-.38.09-.73.22-1.04.42v-2.62h4.9Z" fill-rule="evenodd" clip-rule="evenodd" fill="currentColor" />
+    </svg>
+}
+
+export const L4 = ({ style }: { style?: CSSProperties | undefined }) => {
+    return <svg style={style} width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <rect width="32" height="32" fill="none"/>
+    <path d="m2,0C.9,0,0,.9,0,2v28c0,1.1.9,2,2,2h28c1.1,0,2-.9,2-2V2c0-1.1-.9-2-2-2H2Zm5.67,22h7.52v-2.11h-5.17v-9.09h-2.35v11.2Zm16.61-4.35v-6.85h-2.18l-5.49,7.12v1.54h5.28v2.54h2.38v-2.54h1.62v-1.81h-1.62Zm-5.38,0l3.09-4.05v4.05h-3.09Z" fill-rule="evenodd" clip-rule="evenodd"fill="currentColor"/>
+  </svg>
+}
